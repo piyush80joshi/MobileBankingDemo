42fccaf38c294acaed792c59a16b051a856e5934
piyush80joshi <piyush80joshi@gmail.com>
2016-11-21 23:07:15 -0500

    updated Transfer Balance to add amount field

--- app/src/main/java/com/cts/mobilebankingdemo/BalanceTransferActivity.java
+++ app/src/main/java/com/cts/mobilebankingdemo/BalanceTransferActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,23 +17,28 @@ import com.cts.mobilebankingdemo.service.BankingApplication;
 import java.util.ArrayList;
 
 public class BalanceTransferActivity extends AppCompatActivity {
-    private Spinner acctFromSpn ;
-    private Spinner acctToSpn ;
+    private Spinner acctFromSpn;
+    private Spinner acctToSpn;
     private TextView tTransferLogOut;
     private TextView tFromAcct;
     private TextView tToAcct;
-    private Button bBack;
+    private Button bCancel;
     private Button bTransferBalance;
     private TextView tUser;
     private String userName;
+    private EditText eAmount;
+    private String selectedFromAccount;
+    private String selectedToAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_balance_transfer);
-        bTransferBalance =(Button) findViewById(R.id.mTransBalBtn);
+        eAmount = (EditText) findViewById(R.id.mAmountTxt);
+        bTransferBalance = (Button) findViewById(R.id.mTransBalBtn);
         tTransferLogOut = (TextView) findViewById(R.id.mLogoutTxt3);
-        userName=((BankingApplication)getApplicationContext()).getUserName();
-        tUser =(TextView)findViewById(R.id.mUserTxt3);
+        userName = ((BankingApplication) getApplicationContext()).getUserName();
+        tUser = (TextView) findViewById(R.id.mUserTxt3);
         tUser.setText(userName);
         tTransferLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,25 +51,31 @@ public class BalanceTransferActivity extends AppCompatActivity {
         bTransferBalance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               //TODO
-                /* Intent transferBalanceIntent = new Intent(BankingActivity.this, BalanceTransferActivity.class);
-                BankingActivity.this.startActivity(transferBalanceIntent);*/
+                //TODO
+                transferBalance();
+            }
+        });
+
+        bCancel = (Button) findViewById(R.id.mbackBtn);
+        bCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bankingIntent = new Intent(BalanceTransferActivity.this, BankingActivity.class);
+                BalanceTransferActivity.this.startActivity(bankingIntent);
             }
         });
         //-------------------------
-        tFromAcct=(TextView)findViewById(R.id.mAcctFromTxt);
-        tToAcct=(TextView)findViewById(R.id.mAcctToTxt);
-        ArrayList<String> fromAccountList = new ArrayList<String>();
-        fromAccountList.add("Account 1");
-        fromAccountList.add("Account 2");
-        ArrayAdapter fromAcctAdapter=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,fromAccountList);
+        tFromAcct = (TextView) findViewById(R.id.mAcctFromTxt);
+        tToAcct = (TextView) findViewById(R.id.mAcctToTxt);
+        ArrayList<String> fromAccountList = getFromAccountList();
+        ArrayAdapter fromAcctAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, fromAccountList);
 
-        acctFromSpn = (Spinner)findViewById(R.id.mActFromSpn);
+        acctFromSpn = (Spinner) findViewById(R.id.mActFromSpn);
         acctFromSpn.setAdapter(fromAcctAdapter);
         acctFromSpn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getBaseContext(),adapterView.getItemAtPosition(i)+" Selected ",Toast.LENGTH_LONG);
+                selectedFromAccount = adapterView.getItemAtPosition(i).toString();
             }
 
             @Override
@@ -72,16 +84,14 @@ public class BalanceTransferActivity extends AppCompatActivity {
             }
         });
 
-        ArrayList<String> toAccountList = new ArrayList<String>();
-        toAccountList.add("To Account 1");
-        toAccountList.add("To Account 2");
-        ArrayAdapter toAcctAdapter=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,toAccountList);
-        acctToSpn = (Spinner)findViewById(R.id.mAcctToSpn);
+        ArrayList<String> toAccountList = getToAccountList();
+        ArrayAdapter toAcctAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, toAccountList);
+        acctToSpn = (Spinner) findViewById(R.id.mAcctToSpn);
         acctToSpn.setAdapter(toAcctAdapter);
         acctToSpn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getBaseContext(),adapterView.getItemAtPosition(i)+" Selected ",Toast.LENGTH_LONG);
+                selectedToAccount = adapterView.getItemAtPosition(i).toString();
             }
 
             @Override
@@ -91,5 +101,27 @@ public class BalanceTransferActivity extends AppCompatActivity {
         });
 
     }
+
+    private ArrayList<String> getToAccountList() {
+        ArrayList toAccountList = new ArrayList<String>();
+        toAccountList.add("Ritam.B");
+        toAccountList.add("Mani");
+        return toAccountList;
+    }
+
+    private ArrayList<String> getFromAccountList() {
+        ArrayList fromAccountList = new ArrayList<String>();
+        fromAccountList.add(userName);
+
+        return fromAccountList;
+    }
+
+    private void transferBalance() {
+        //TODO
+
+        String toastString = "Amount $" + eAmount.getText().toString() + " transferred from Account " + selectedFromAccount + " to Account " + selectedToAccount + " .";
+        Toast.makeText(getBaseContext(), toastString, Toast.LENGTH_LONG).show();
+    }
+
 
 }
